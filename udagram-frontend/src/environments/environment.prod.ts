87980ea7a9b4cd7114ce8b107fbc779a838a590f
tingyid <tingyi.dong2010@gmail.com>
2022-01-17 22:50:21 -0500

    update external IP

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a176907157acc48ebae2789278f2be93-104599682.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'a176907157acc48ebae2789278f2be93-104599682.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
